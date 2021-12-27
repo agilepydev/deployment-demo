50596d84867f9dc9440f849b37a7356d8bf90be6
agilepydev <ikmaryam@hotmail.com>
2021-12-27 15:31:51 +0100

    Dash

--- app/main.py
+++ app/main.py
@@ -12,25 +12,39 @@ app = dash.Dash()
 
 
 app.layout = html.Div(children=[
+    dcc.Link('Navigate to "/"', href='index.html'),
     html.H1(children='Games Dashboard'),
-    dcc.Dropdown(id='features-dropdown',
-                 options=[{'label': i, 'value': i}
-                          for i in df['currency'].unique()],
-                 value= "EUR"),
-    dcc.Graph(id='revenue-graph')
+    dcc.Dropdown(id='features1-dropdown',
+                 options=[{'label': 'revenue', 'value': 'revenue'},
+                          {'label': 'price', 'value': 'price'}],
+                          
+                 value= "currency"),
+    
+    dcc.Dropdown(id='features2-dropdown',
+                 options=[{'label': 'revenue', 'value': 'revenue'},
+                          {'label': 'price', 'value': 'price'}],
+                          
+                 value= "currency"),
+    dcc.Graph(id='graph')
 ])
 
+
+
+
 @app.callback(
-    Output(component_id='revenue-graph', component_property='figure'),
-    Input(component_id='features-dropdown', component_property='value')
+    Output(component_id='graph', component_property='figure'),
+    Input(component_id='features1-dropdown', component_property='value'),
+    Input(component_id='features2-dropdown', component_property='value')
 )
-def update_graph(selected_currency):
-    filtered_games = df[df['currency'] == selected_currency]
-    line_fig = px.scatter(filtered_games,
-                       x='release date', y='revenue',
-                       color='currency',
-                       title=f'Game Revenue in {selected_currency}')
-    return line_fig
+def update_graph(val1, val2):
+
+    x = df[val1].iloc[:10]
+    y = df[val2].iloc[:10]
+
+    fig =px.bar(df,x= x, y=y)
+    
+    return fig
+
 
 
 if __name__ == '__main__':
